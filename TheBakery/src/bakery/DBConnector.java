bf50041f50924bafb9654436df0493ed7028824e
spydon <lukas.klingsbo@gmail.com>
2014-05-28 15:30:56 +0200

    Added files for database connection

---
+++ TheBakery/src/bakery/DBConnector.java
@@ -0,0 +1,28 @@
+package bakery;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public abstract class DBConnector {
+    protected String url = "jdbc:mysql://localhost/bakery";
+    protected String dbUser = "root";
+    protected String pass = "YouWillNeverRealizeHowFuckingLongThisPasswordIs!";
+
+    protected Connection getConnection() {
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+            conn = DriverManager.getConnection(url, dbUser, pass);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+        return conn;
+    }
+}
